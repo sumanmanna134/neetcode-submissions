49b5f4d99c2b397f3c1f7bdf896c62565c923c29
Suman Manna <manna.suman134@gmail.com>
2026-07-30 22:58:05 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.java
@@ -0,0 +1,32 @@
+class MinStack {
+    Stack<Integer> stack = new Stack<>();
+    Stack<Integer> minStack = new Stack<>();
+
+    public MinStack() {
+
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if(minStack.isEmpty() || val<=minStack.peek()){
+            minStack.push(val);
+        }
+        
+    }
+    
+    public void pop() {
+        int current = stack.pop();
+        if(minStack.peek()==current){
+            minStack.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+        
+    }
+}
